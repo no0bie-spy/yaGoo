9d19d8278ba3e20b9c86dafe4c508dd39acd563a
KrishnaGrg1 <gkrishnabahadur618@gmail.com>
2025-04-08 17:42:35 +0545

    maintained frontend

---
+++ frontend/app/explore.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+
+export default function expolre() {
+    return (
+      <View className="flex-1 justify-center items-center bg-blue-500">
+        <Text className="text-white text-2xl">Hello, Tailwind</Text>
+      </View>
+    );
+  }
+  
