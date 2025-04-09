1df2eec9d85da69a1a610620f7d3fe3f48be0796
KrishnaGrg1 <gkrishnabahadur618@gmail.com>
2025-04-09 10:25:24 +0545

    initailized the emulator

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,25 +1,36 @@
-import { View, Text } from 'react-native';
-import "./global.css"
-import { Link } from 'expo-router';
-import { blue } from 'react-native-reanimated/lib/typescript/Colors';
-import { FlipInEasyX } from 'react-native-reanimated';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+const App = () => {
+  const [text, setText] = useState('Hello, React Native!');
 
-export default function App() {
+  const changeText = () => {
+    setText('You pressed the button!');
+  };
+  const updateText=()=>{
+    setText("")
+  }
+
   return (
-    <View style={
-      {
-        display:'flex',
-        justifyContent:'center',
-        alignItems:'center',
-       
-      }
-    } className="flex-1 justify-center items-center bg-blue-500">
-      <Text className="text-white text-2xl">Hello, Tailwind</Text>
-      <Text>hello there</Text>
-      <Link href={"/explore"}> 
-        <Text>Go to explore</Text>
-      </Link>
+    <View style={styles.container}>
+      <Text style={styles.text}>{text}</Text>
+      <Button title="Press me" onPress={changeText} />
+      <Button title='Press to change' onPress={updateText} />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+});
+
+export default App;
