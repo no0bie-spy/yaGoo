eacf0ff367ecd7d9a4faad4b36208f0e74423c05
suyanthapa <146444078+suyanthapa@users.noreply.github.com>
2025-04-10 14:49:16 +0545

    Model OTP

--- backend/models/otp.js
+++ backend/models/otp.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 // import User from './users.js';
 
 const otpSchema = new mongoose.Schema({
-  email: { type: String,ref: "User", unique: true, required: true },
+  email: { type: mongoose.Schema.Types.ObjectId,ref: "User", unique: true, required: true },
   otp: { type: String, required: true },
   otpExpiresAt: { type: Date, required: true },
   createdAt: { type: Date, default: Date.now },
